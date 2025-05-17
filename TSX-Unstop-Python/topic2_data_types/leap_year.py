c92bdf7c710b75faceb52b752c4ebc26b410c36a
yashmitagandepalli <yashmitagandepalli806@gmail.com>
2025-05-17 23:25:24 +0530

    leap_year.py

---
+++ TSX-Unstop-Python/topic2_data_types/leap_year.py
@@ -0,0 +1,7 @@
+# Check if a year is a leap year
+year = int(input("Enter a year: "))
+
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print(year, "is a leap year.")
+else:
+    print(year, "is not a leap year.")
