e3eb445651bcd372b2a9fc61d4a5a9ff882027f2
yashmitagandepalli <yashmitagandepalli806@gmail.com>
2025-05-17 23:26:19 +0530

    rectangle_area_perimeter.py

---
+++ TSX-Unstop-Python/topic2_data_types/rectangle_area_perimeter.py
@@ -0,0 +1,9 @@
+# Calculate the area and perimeter of a rectangle
+length = 5
+width = 3
+
+area = length * width
+perimeter = 2 * (length + width)
+
+print("Area:", area)
+print("Perimeter:", perimeter)
