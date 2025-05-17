c3c1f50b616c9da39e3939a4170d5d3a7d437eb2
yashmitagandepalli <yashmitagandepalli806@gmail.com>
2025-05-17 23:25:04 +0530

    compare_numbers.py

---
+++ TSX-Unstop-Python/topic2_data_types/compare_numbers.py
@@ -0,0 +1,10 @@
+# Compare two numbers entered by the user
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+if a > b:
+    print("First number is greater.")
+elif a < b:
+    print("First number is smaller.")
+else:
+    print("Both numbers are equal.")
